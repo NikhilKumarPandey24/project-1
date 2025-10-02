61fbcc085231da087f2a1212de1cb92074c401e8
NikhilKumarPandey24 <iamnkpiamsanu@gmail.com>
2025-10-03 00:12:21 +0530

    done

--- mainshortend.py
+++ mainshortend.py
@@ -17,17 +17,22 @@ if computer==you:
     print("It's a tie")
 
 else:
-    if computer ==-1 and you==1:
+    '''if computer ==-1 and you==1: (python-you ) == -2
         print("You win")
-    elif computer==1 and you==0:
+    elif computer==-1 and you==0: (python-you ) == -1
         print("You lose")
-    elif computer==1 and you==-1:
+    elif computer==1 and you==-1: (python-you ) == 2    
         print("You lose")
-    elif computer==1 and you==0:
+    elif computer==1 and you==0: (python-you ) == 1
         print("You win")
-    elif computer==0 and you==-1:
+    elif computer==0 and you==-1: (python-you ) == 1
         print("You win")
-    elif computer==0 and you==1:
+    elif computer==0 and you==1:    (python-you ) == -1
+        print("You lose")
+        
+        the below logic is written on the basis of the difference between computer and you'''
+
+    if (computer-you == -1) or (computer-you == 2):
         print("You lose")
     else:
-        print("Invalid input")
+        print("You win")
